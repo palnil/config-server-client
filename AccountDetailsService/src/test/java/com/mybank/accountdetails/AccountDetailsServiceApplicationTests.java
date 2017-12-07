9c6e95c9680e1dc576cf85c1ae75744425d99ce5
Nilesh Patel <palnil@gmail.com>
2017-12-06 19:44:00 -0500

    initial checkin

---
+++ AccountDetailsService/src/test/java/com/mybank/accountdetails/AccountDetailsServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mybank.accountdetails;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountDetailsServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ ConfigServer/src/main/java/com/yoshallc/configserver/ConfServerApplication.java
@@ -0,0 +1,14 @@
+package com.yoshallc.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfServerApplication.class, args);
+	}
+}
